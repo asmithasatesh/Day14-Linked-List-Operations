5bc6dd60b78ff85cd44c6a54c02141e9184b7da9
asmithasatesh <asmithasatesh@gmail.com>
2021-07-10 10:00:31 +0530

    [Asmitha]Create Linked List

--- LinkedListOperation/Program.cs
+++ LinkedListOperation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinkedListOperations;
+using System;
 
 namespace LinkedListOperation
 {
@@ -6,7 +7,19 @@ namespace LinkedListOperation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List!");
+            Console.WriteLine("Welcome to Linked List Program!");
+            OperationsOnLinkedList obj = new OperationsOnLinkedList();
+            Console.WriteLine("Enter 1 to Add at Last");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    obj.InsertLast(56);
+                    obj.InsertLast(30);
+                    obj.InsertLast(70);
+                    obj.Display();
+                    break;
+            }
         }
     }
 }
